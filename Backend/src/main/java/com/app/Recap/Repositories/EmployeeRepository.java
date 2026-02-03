c561e22aa55247a522db38c0d9a2fbae69d6ac1f
Sandip Salve <sandip.salve@synthesys.co.in>
2026-02-03 23:10:02 +0530

    server side pagination API development completed and tested on postman

--- Backend/src/main/java/com/app/Recap/Repositories/EmployeeRepository.java
+++ Backend/src/main/java/com/app/Recap/Repositories/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.app.Recap.Repositories;
 
 import com.app.Recap.Entities.Employee;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +12,6 @@ import java.util.List;
 public interface EmployeeRepository extends JpaRepository<Employee,Long> {
 
     List<Employee> findByEmployeeNameContainingIgnoreCase(String employeeName);
+
+    Page<Employee> findAll(Pageable pageableObj);
 }
