de4db2434f06f119c3422986946f29e1216b066d
Sandip Salve <sandip.salve@synthesys.co.in>
2026-02-04 22:52:01 +0530

    Commit for Pagination in Angular hanled using ngx-pagination.

--- FrontEnd/src/app/Service/employee.service.ts
+++ FrontEnd/src/app/Service/employee.service.ts
@@ -14,11 +14,14 @@ export class EmployeeService {
     return this.httpClient.get<any[]>(`${this.baseURL}/list`);
   }
 
-  getSearchResult(value:any):Observable<any[]>{
-    return this.httpClient.get<any[]>(`${this.baseURL}/search?employeeName=${value}`);
+  getSearchResult(value?:any,pageNumber?:number,pageSize?:number,sortBy?:string,sortingDirection?:string):Observable<any[]>{
+    // return this.httpClient.get<any[]>(`${this.baseURL}/search?employeeName=${value}`);
+    return this.httpClient.get<any>(`${this.baseURL}/getPaginatedEmployees?pageNumber=${pageNumber}&pageSize=${pageSize}&sortBy=${sortBy}&sortingDirection=${sortingDirection}`);
   }
 
   updateEmployeeDetails(id:number,employee:any){
     return this.httpClient.post<any>(`${this.baseURL}/${id}`,employee);
   }
+
+
 }
